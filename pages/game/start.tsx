6883566dd6e978811ca26dcf6558a7209bd7f5f5
auleki <kodagiwa@gmail.com>
2022-11-29 08:00:47 +0100

    update location for layout component

--- pages/game/start.tsx
+++ pages/game/start.tsx
@@ -1,7 +1,10 @@
+import LinkButton from "../../components/buttons/LinkButton"
+
 const Start = () => {
   return (
     <div>
       <h1>Start Game</h1>
+      <LinkButton path="/playing" pathname="Finish Game" />
     </div>
   )
 }
