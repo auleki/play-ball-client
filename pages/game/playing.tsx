68e18481d7d886ebbfd36bafd296bdd3c0e64671
auleki <kodagiwa@gmail.com>
2023-01-12 00:52:02 +0100

    populate playing page

--- pages/game/playing.tsx
+++ pages/game/playing.tsx
@@ -1,12 +1,40 @@
-import React from 'react'
+import React, { useState } from 'react'
+import Button from '../../components/buttons/Button'
 import LinkButton from '../../components/buttons/LinkButton'
 import PageLayout from '../../components/layout/PageLayout'
+import Scoreboard from '../../components/macros/Scoreboard'
 
 const Playing = () => {
+    const [teamA, setTeamA] = useState({
+        name: "UNEC Ballers",
+        points: 42
+    })
+    const [teamB, setTeamB] = useState({
+        name: "Stadium Boys",
+        points: 6
+    })
+
+    function scoreBasket(team: string, points: number) {
+        switch (team) {
+            case "A":
+                setTeamA(team => ({ ...team, points: Number(team.points) + points }))
+                break
+            case "B":
+                setTeamB(team => ({ ...team, points: Number(team.points) + points }))
+            default:
+                "No valid team selected"
+        }
+    }
+
     return (
         <PageLayout>
-            <h1>Play State</h1>
-            <LinkButton path='/game/over' pathname='Finish game' />
+            <div className='flex flex-col gap-8'>
+                <h1 className="text-white">Play State</h1>
+                <Scoreboard teamA={teamA} teamB={teamB} />
+                <Button onClick={() => scoreBasket("A", 3)} text="Team A Score" />
+                <Button onClick={() => scoreBasket("B", 3)} text="Team B Score" />
+                <LinkButton path='/game/over' pathname='Finish game' />
+            </div>
         </PageLayout>
     )
 }
