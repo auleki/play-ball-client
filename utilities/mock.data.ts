311693c4663f10fa30ebb66ce0aeea8f8ba2bc42
auleki <kodagiwa@gmail.com>
2023-01-12 00:52:39 +0100

    mock data to test draft page

---
+++ utilities/mock.data.ts
@@ -0,0 +1,26 @@
+export const teams = [
+    {
+        id: 34,
+        name: "Vikings Menace",
+        starters: 5,
+        bench: 3
+    },
+    {
+        id: 21,
+        name: "UNEC Ballers",
+        starters: 5,
+        bench: 1
+    },
+    {
+        id: 14,
+        name: "82 Div Boys",
+        starters: 5,
+        bench: 2
+    },
+    {
+        id: 91,
+        name: "Lemon Peel",
+        starters: 5,
+        bench: 2
+    }
+]
