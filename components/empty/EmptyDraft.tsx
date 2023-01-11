f48df886e94db6c1c43b2afdac42291b15cd3af2
auleki <kodagiwa@gmail.com>
2023-01-12 00:49:20 +0100

    empty screen for Drafts

--- components/empty/EmptyDraft.tsx
+++ components/empty/EmptyDraft.tsx
@@ -4,7 +4,7 @@ import RoyalButton from '../buttons/RoyalButton'
 const EmptyDraft = () => {
     return (
         <div className="mt-32 text-center flex flex-col items-center gap-4 w-5/6">
-            <h4 className="text-2xl">No Teams</h4>
+            <h4 className="text-2xl text-white">No Teams</h4>
             <p className="text-sm text-gray-200">
                 To start the set series you need to
                 include at least <span className="text-red-300 font-bold">3</span> teams
