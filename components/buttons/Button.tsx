d32cd24a95d103e4c5f891b3a3712b628c4ee407
auleki <kodagiwa@gmail.com>
2023-01-12 00:47:41 +0100

    enable button to accept onClick events

--- components/buttons/Button.tsx
+++ components/buttons/Button.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-const Button = ({ text }: { text: string }) => {
+const Button = ({ text, onClick }: { text: string, onClick?: () => void }) => {
     return (
-        <button>{text}</button>
+        <button onClick={onClick} className='px-4 min-w-max py-2 rounded-md bg-green-600 text-white'>{text}</button>
     )
 }
 
